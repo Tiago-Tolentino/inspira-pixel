6aff5a82020490c3be3a504f75d94ffb614a75d6
Tiago-Tolentino <tiagotolentino11@gmail.com>
2025-12-03 09:43:28 -0300

    atualizacoes

---
+++ src/components/Banner.vue
@@ -0,0 +1,37 @@
+<script setup>
+import fotografo from "../assets/fotografo.png"
+</script>
+
+<template>
+    
+    <section class="banner">
+        <div>
+        <h1>Onde a paisagem vira 
+        <span>inspiração</span>.</h1>
+        <p>Explore paisagens, cores e luzes que inspiram novas histórias através da fotografia.</p>
+        </div>
+        <img :src="fotografo" alt="fotografo banner">
+    </section>
+
+</template>
+
+<style scoped lang="scss">
+    .banner{
+        display: flex;
+        justify-content: space-between;
+        margin: 2rem;
+        span{
+            color: red;
+            font-size: 4.5rem;
+        }
+    }
+
+    div{
+        border: solid;
+        width: 70vw;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,56 @@
 <script setup>
 import { Icon } from "@iconify/vue";
 import logo from "../assets/logo.png"
+import search from "../assets/button-search.png"
+import user from "../assets/button-user.png"
 </script>
 
 <template>
-    <img src="logo" alt="logo">
-    <nav>
+<header>
+    <img :src="logo" alt="logo">
+    <ul>
         <li>
-            <a href="">
-                 <Icon class="search-icon" icon="material-symbols:search" />
-            </a>
+            <img :src="search" alt="icone de pesquisa">
         </li>
-    </nav>
+        <li>
+            <a href="">Inicio</a>
+        </li>
+        <li>
+            <a href="">Galeria</a>
+        </li>
+        <li>
+            <a href="">Favoritos</a>
+        </li>
+        <li>
+            <img :src="user" alt="icone de usuario">
+        </li>
+    </ul>
+</header>
 </template>
 
 <style scoped lang="scss">
+header{
+    //border: solid;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    margin: 2rem;
+
+    ul{
+        display: flex;
+        gap: 1.5rem;
+       // border: solid;
+        flex-wrap: wrap;
+        list-style: none;
+        align-items: center;
+    }
+
+    a{
+        text-decoration: none;
+        color: black;
+    }
+}
 
 </style>
 
